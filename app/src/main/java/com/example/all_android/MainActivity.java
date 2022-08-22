e4626bdc5aaa3bf6e13f6be52f63f7065ab2ec5e
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2022-08-22 10:22:07 +0530

    Album

--- app/src/main/java/com/example/all_android/MainActivity.java
+++ app/src/main/java/com/example/all_android/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.all_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView1,imageView2,imageView3;
+    Button next;
+    Button pre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
