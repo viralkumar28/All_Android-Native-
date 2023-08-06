6bd2449b33bd0b68918744d430fd3f6df3462119
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2023-08-06 16:27:04 +0530

    Added New Files

---
+++ app/src/main/java/com/example/all_android/BroadCastReveiver.java
@@ -0,0 +1,19 @@
+package com.example.all_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class BroadCastReveiver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broad_cast_reveiver);
+
+
+    }
+}
--- app/src/main/java/com/example/all_android/ImpliciteIntents.java
+++ app/src/main/java/com/example/all_android/ImpliciteIntents.java
@@ -2,13 +2,32 @@ package com.example.all_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ImpliciteIntents extends AppCompatActivity {
-
+        Button share;
+        TextView quote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_implicite_intents);
+
+        share = findViewById(R.id.share);
+        quote = findViewById(R.id.textViewQuote);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sharetxt = quote.getText().toString();
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, sharetxt);
+                startActivity(intent);
+            }
+        });
     }
 }
