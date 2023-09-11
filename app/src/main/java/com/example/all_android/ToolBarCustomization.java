345480c0d034ef33f6c77d9c662c702c389d931d
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2023-09-11 12:22:33 +0530

    Added New Files

---
+++ app/src/main/java/com/example/all_android/ToolBarCustomization.java
@@ -0,0 +1,13 @@
+package com.example.all_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToolBarCustomization extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool_bar_customization);
+    }
+}
